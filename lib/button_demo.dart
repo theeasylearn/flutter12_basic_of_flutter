1c759edc050f701c664c2283cb609f1d325328f2
theeasylearn <theeasylearn@gmail.com>
2026-01-29 17:57:51 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/gridview_example_1.dart

--- lib/button_demo.dart
+++ lib/button_demo.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:fluttertoast/fluttertoast.dart';
 class ButtonDemo extends StatelessWidget {
   int count = 0; //instance variable
   @override
@@ -25,10 +25,23 @@ class ButtonDemo extends StatelessWidget {
                 textColor: Colors.white,
                 color: Colors.blueAccent,
                 onPressed: (){
-                  //display toast message
+                  Fluttertoast.showToast(
+                    msg: "Hello! Dishant, Manav & harshraj and hardik ",
+                    toastLength: Toast.LENGTH_LONG,    // or Toast.LENGTH_LONG
+                    gravity: ToastGravity.BOTTOM,       // BOTTOM, CENTER, TOP
+                    timeInSecForIosWeb: 3,
+                    backgroundColor: Colors.black,
+                    textColor: Colors.white,
+                    fontSize: 16.0,
+                    webShowClose: true,                 // useful for web
+                  );
                 },
                 child: Text("Click me to display toast message"),
-              )
+              ),
+              SizedBox(height: 10,),
+              IconButton(onPressed: () {
+
+              }, icon: Icon(Icons.message)),
             ],
         ),
       ),
